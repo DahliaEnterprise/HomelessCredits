be9338b17810a043423c750165e119ce0c436e1d
DahliaEnterprise <35324244+DahliaEnterprise@users.noreply.github.com>
2023-07-28 14:05:23 -0700

    Create homelesscredits_server_authorize_first_parcela.php

---
+++ homelesscredits_server_authorize_first_parcela.php
@@ -0,0 +1,32 @@
+<?php
+$host = '127.0.0.1';
+$db   = 'homelesscredits';
+$user = 'uptweet';
+$pass = 'fuckthat';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+$pdo = new PDO($dsn, $user, $pass, $options);
+
+$return_json_object_as_array = [];
+$return_json_object_as_array["parcela_available"] = -1;
+$stmt = $pdo->query('SELECT result_hash, timestamp FROM parcela ORDER BY timestamp DESC LIMIT 0,1;');
+$row = $stmt->fetch();
+if($stmt->rowCount() > 0)
+{
+    //define output
+    $return_json_object_as_array["parcela_available"] = 1;
+    $return_json_object_as_array["result_hash"] = $row["result_hash"];
+    $return_json_object_as_array["timestamp"] = $row["timestamp"];
+}else if($stmt->rowCount() == 0)
+{
+    $return_json_object_as_array["parcela_available"] = 0;
+}
+
+echo json_encode($return_json_object_as_array);
+?>
