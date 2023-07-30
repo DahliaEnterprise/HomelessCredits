f011325cd8f82b35503e0028f2894a124ed5e78e
Shane Betz <dahliacommunication67@gmail.com>
2023-07-30 04:29:37 -0700

    *Moved functions into a worker intended script

--- homelesscredits_client.php
+++ homelesscredits_client.php
@@ -4,117 +4,13 @@
     <script>
         //Global
             //Constants
-            const alphabet_table = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
             //Generate Credits States
             var generate_credits_last_percela_informacion = [];
 
 
         //Functions
-        //Rotate character based on iterations amongst the alphabet table
-        function rotate_character(character, iterations)
-        {
-            let output = -1;
 
-            //Determine current index amongst the alphabet table.
-            let current_character_index = -1;
-            //Execute
-            let index = 0;
-            while(index < alphabet_table.length)
-            {
-                if(alphabet_table[index] === character)
-                {
-                    current_character_index = index;
-                    index = alphabet_table.length;
-                }else{
-                    //Next character
-                    index = index + 1;
-                }
-            }
-
-            //Fault occured(No allowed character found)?
-            if(current_character_index > -1)
-            {
-                //No fault occured
-                //Execute Rotate Character
-                let rotate_character_index = current_character_index;
-                let iterations_consumed = 0;
-                let continue_rotating = 1;
-                while(continue_rotating == 1)
-                {
-                    rotate_character_index = rotate_character_index + 1;
-                    //Continue loop through alphabet table
-                    if(rotate_character_index == alphabet_table.length)
-                    {
-                        rotate_character_index = 0;
-                    }
-
-                    iterations_consumed = iterations_consumed + 1;
-
-                    if(iterations_consumed == iterations)
-                    {
-                        output = rotate_character_index;
-                        continue_rotating = 0;
-                    }
-                }
-            }
-
-            //Return
-            return output;
-        }
-
-        function rotate_string_swap_character_per_character_position(string_to_rotate, iterations)
-        {
-            let output = "";
-
-            //Execute rotate each character per character position
-            let index = 0;
-            while(index < string_to_rotate.length)
-            {
-                let exponential_rotation_strength = (index + 2) * iterations;//Ensure it is always above 1 to enable randomness
-                let new_character_index = rotate_character(string_to_rotate[index], exponential_rotation_strength);
-                output = output + "" + alphabet_table[new_character_index];
-                index = index + 1;
-            }
-
-            return output;
-        }
-
-        //Random Assistive Functions
-        function generate_random_index_amongst_alphabet_table()
-        {
-            let output = -1;
-
-            let keep_generating = 1;
-            while(keep_generating == 1)
-            {
-                const array_for_one_random_integer = new Uint8Array(1);
-                self.crypto.getRandomValues(array_for_one_random_integer);
-
-                if(array_for_one_random_integer[0] < alphabet_table.length)
-                {
-                    keep_generating = 0;
-                    output = array_for_one_random_integer[0];
-                }
-
-
-            }
-
-            return output;
-        }
-
-        function generate_random_string(string_length_to_generate)
-        {
-            let output = "";
-            let total_generated = 0;
-            while(total_generated < string_length_to_generate)
-            {
-                output = output + "" + alphabet_table[generate_random_index_amongst_alphabet_table()];
-                total_generated = total_generated + 1;
-            }
-
-            return output;
-        }
 
         function get_balance_from_server()
         {
@@ -137,6 +33,7 @@
                     {
                         let information_for_generatecredits_worker = [];
                         information_for_generatecredits_worker["first_parcela"] = 1;
+                        information_for_generatecredits_worker["difficulty"] = 1;
                         generatecredits_worker = new Worker("/homelesscredits_generate_credits_worker.js");
                         //Send the worker a message
                         generatecredits_worker.postMessage(information_for_generatecredits_worker);
